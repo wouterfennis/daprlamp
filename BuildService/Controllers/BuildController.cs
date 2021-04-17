9c4d0891e3ed89ecbdd4c007d9ec6233f8a56d27
Moeluh <dylanmolenaar@hotmail.nl>
2021-04-17 11:07:33 +0200

    Cleaned asp.net core project

---
+++ BuildService/Controllers/BuildController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace BuildService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BuildController : ControllerBase
+    {
+        private readonly ILogger<BuildController> _logger;
+
+        public BuildController(ILogger<BuildController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public ActionResult Webhook()
+        {
+            // Convert json to event and publish to topic using dapr
+            return Ok();
+        }
+    }
+}
